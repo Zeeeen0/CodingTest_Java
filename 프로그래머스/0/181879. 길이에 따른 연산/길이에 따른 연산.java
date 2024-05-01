a493e3742978841c922bd5774b28f25d930c55ea
Zeeeen0 <109052080+Zeeeen0@users.noreply.github.com>
2024-05-02 01:18:55 +0900

    [level 0] Title: 길이에 따른 연산, Time: 0.01 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] num_list) {
+        if (num_list.length >= 11) return Arrays.stream(num_list).sum();
+        
+        int answer = 1;
+        for (int num : num_list) {
+            answer *= num;
+        }
+        return answer;
+    }
+}
