0500cdf652ff6b0231c52b6b2512dc7a94449770
Zeeeen0 <109052080+Zeeeen0@users.noreply.github.com>
2024-05-02 15:56:04 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 5.60 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class Solution {
+    public String solution(String my_string) {
+        return Stream.of(my_string.toLowerCase().split(""))
+                .sorted()
+                .collect(Collectors.joining());
+    }
+}
