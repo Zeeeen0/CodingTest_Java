89b88d4a7ca82011e6e5a2d7bbece90cd090318a
Zeeeen0 <109052080+Zeeeen0@users.noreply.github.com>
2024-05-09 15:49:45 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.71 ms, Memory: 56.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,51 +1,55 @@
 import java.util.*;
 
 class Solution {
-    int[] dx = {1, 0, -1, 0};
-    int[] dy = {0, 1, 0, -1};
-    
+    int[][] D = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    class Point {
+        int x, y, dist;
+        Point(int x, int y, int dist) {
+            this.x = x;
+            this.y = y;
+            this.dist = dist;
+        }
+    }
+
     public int solution(int[][] maps) {
-        int answer = 0;
+
+        return bfs(maps, 0, 0);
+    }
+
+    private int bfs(int[][] maps, int x, int y) {
         int n = maps.length;
         int m = maps[0].length;
-        
-        int[][] visited = new int[n][m];
-        
-        bfs(maps, visited);
-        answer = visited[n - 1][m - 1];
-        
-        if (answer == 0) {
-            return -1;
-        }
-        
-        return answer;
-    }
-    
-    private void bfs(int[][] maps, int[][] visited) {
-        int x = 0;
-        int y = 0;
-        visited[x][y] = 1;
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
-        
+        boolean[][] visited = new boolean[n][m];
+
+        visited[x][y] = true;
+        Queue<Point> queue = new LinkedList<>();
+        queue.add(new Point(x, y, 1));
+
         while (!queue.isEmpty()) {
-            int[] current = queue.remove();
-            int cx = current[0];
-            int cy = current[1];
+            Point current = queue.remove();
+            int cx = current.x;
+            int cy = current.y;
             
+            // 도착지점인 경우 dist 리턴
+            if (cx == n - 1 && cy == m - 1) {
+                return current.dist;
+            }
+
             for (int i = 0; i < 4; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
+                int nx = cx + D[i][0];
+                int ny = cy + D[i][1];
                 
-                if (nx < 0 || ny < 0 || nx > maps.length - 1 || ny > maps[0].length - 1) {
-                    continue;
-                }
+                if (nx < 0 || nx > n - 1 || ny < 0 || ny > m - 1) continue; // 배열 외 값
+                if (visited[nx][ny]) continue; // 이미 방문
+                if (maps[nx][ny] == 0) continue; // 벽
                 
-                if (visited[nx][ny] == 0 && maps[nx][ny] == 1) {
-                    visited[nx][ny] = visited[cx][cy] + 1;
-                    queue.add(new int[]{nx, ny});
-                }
+                visited[nx][ny] = true;
+                queue.add(new Point(nx, ny, current.dist + 1));
             }
         }
+        
+        // 도착하지 못할 경우 -1 리턴
+        return -1;
     }
 }
